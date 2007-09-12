2b2222a56d0d0dee1acb4e235908bfb2d6aa5faa
nleutner <info@example.com>
2007-09-12 16:24:56 +0000

    git-svn-id: http://svn.projects.ez.no/gisoperators@9 21389c0b-60da-4233-ab99-b6e3b259ffc7

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class gisoperatorsInfo
     {
         return array(
             'Name' => "<a href='http://projects.ez.no/gisoperators'>Global Information Systems Operators</a>",
-            'Version' => "0.0.1",
+            'Version' => "0.0.2",
             'Copyright' => "Copyright (C) 2007, <a href='http://www.all2e.com/'>all2e GmbH</a>",
             'Author' => "Norman Leutner",
             'License' => "GNU General Public License v2.0"
